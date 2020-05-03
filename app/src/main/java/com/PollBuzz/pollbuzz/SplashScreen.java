cf7d1ce940e958182f130655228edc7f4436bfcc
medhavi <medhavi.srivastava16@gmail.com>
2020-05-03 22:05:21 +0530

    [Medhavi] Added icons and changed splash screen

--- app/src/main/java/com/PollBuzz/pollbuzz/SplashScreen.java
+++ app/src/main/java/com/PollBuzz/pollbuzz/SplashScreen.java
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
@@ -28,6 +29,8 @@ import com.PollBuzz.pollbuzz.LoginSignup.LoginSignupActivity;
 import com.PollBuzz.pollbuzz.LoginSignup.ProfileSetUp;
 import com.PollBuzz.pollbuzz.Utils.firebase;
 import com.PollBuzz.pollbuzz.Utils.helper;
+import com.PollBuzz.pollbuzz.results.PercentageResult;
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
@@ -43,6 +46,7 @@ import java.net.UnknownHostException;
 public class SplashScreen extends AppCompatActivity {
 
     ProgressBar splashProgress;
+
     int SPLASH_TIME = 3000; //3 seconds
     FirebaseAnalytics mFirebaseAnalytics;
     firebase fb;
@@ -58,17 +62,17 @@ public class SplashScreen extends AppCompatActivity {
         FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         fb = new firebase();
-        splashProgress = findViewById(R.id.splashProgress);
-        tv = (TextView) findViewById(R.id.txt);
+        /*splashProgress=findViewById(R.id.splashProgress);
+        splashProgress.setVisibility(View.VISIBLE);*/
         image = (ImageView) findViewById(R.id.imagee);
     }
 
 
-//    private void playProgress() {
-//        ObjectAnimator.ofInt(splashProgress, "progress", 100)
-//                .setDuration(3000)
-//                .start();
-//    }
+   /*private void playProgress() {
+     ObjectAnimator.ofInt(splashProgress, "progress", 100)
+                .setDuration(3000)
+               .start();
+    }*/
 
     public void fade() {
         Animation animation1 =
@@ -77,12 +81,12 @@ public class SplashScreen extends AppCompatActivity {
         image.startAnimation(animation1);
     }
 
-    public void tvanim() {
+    /*public void tvanim() {
         Animation animation2 =
                 AnimationUtils.loadAnimation(getApplicationContext(),
                         R.anim.txt_animation_2);
         tv.startAnimation(animation2);
-    }
+    }*/
 
     private void startIntent() {
         if (!isUserLoggedIn()) {
@@ -163,8 +167,8 @@ public class SplashScreen extends AppCompatActivity {
         dialog.show();
         window.setAttributes(lp);
         splashProgress.setVisibility(View.GONE);
-        tv.getAnimation().cancel();
-        tv.clearAnimation();
+        /*tv.getAnimation().cancel();
+        tv.clearAnimation();*/
         image.getAnimation().cancel();
         image.clearAnimation();
         Button ok = dialog.findViewById(R.id.ok);
@@ -181,9 +185,9 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-//        playProgress();
-        fade();
-        tvanim();
+       //playProgress();
+        //fade();
+        //tvanim();
         boolean f = isInternetAvailable(SplashScreen.this);
         if (!f)
             showDialog();
@@ -191,6 +195,7 @@ public class SplashScreen extends AppCompatActivity {
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
+
                     startIntent();
                 }
             }, SPLASH_TIME);
